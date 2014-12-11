4a7768d5b020b1272fb9fb51a49a293c609eceb0
Ariel Isaacs <ariel.isaacs@gmail.com>
2014-12-11 14:18:56 -0800

    add styling

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
   var $this = $(this);
 
-  $(".pillCard").find(".pillHormone").removeClass("hidden")
+  // $(".pillCard").find(".pillHormone").removeClass("hidden")
+
+  $(".pillCard").mouseover(function(){
+  	console.log("mouse over");
+  	$(this).css("border", "solid black 2px")
+  	$(this).find(".pillHormone").removeClass("hidden")
+
+  })
+
+  $(".pillCard").mouseleave(function(){
+  	$(this).css("border", "none")
+  })
 });
